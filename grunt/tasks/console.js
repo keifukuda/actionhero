66fc5ac54a62dc1894feff7cc281286917fea2f3
福田 景 <kei.fukuda@oliza.jp>
2014-12-12 21:51:00 +0900

    fix prompt

--- grunt/tasks/console.js
+++ grunt/tasks/console.js
@@ -6,33 +6,36 @@ module.exports = function(grunt) {
     var done = this.async();
     grunt.startActionhero(function(api, actionhero){
 
+      prompt = '[ AH::' + api.env + ' ] >> '
       // note this REPL will not run _start commands, only the intilizers
       var r = repl.start({
-        prompt: '',
+        prompt: prompt,
         useGlobal: false
       });
 
       r.on('exit', function(){
-        done();
+        actionhero.stop(function(){
+          done();
+        });
       });
 
       r.outputStream.write('*** STARTING ACTIONHERO REPL ***\r\n\r\n');
 
-      r.prompt = '[ AH::' + api.env + ' ] >> ';
+      // r.prompt = '[ AH::' + api.env + ' ] >> ';
 
-      for(var i in api.config.servers){
-        api.config.servers[i].enabled = false;
-      }
+      // for(var i in api.config.servers){
+      //   api.config.servers[i].enabled = false;
+      // }
 
-      api.config.tasks.scheduler = false;
-      api.config.tasks.queues    = [];
+      // api.config.tasks.scheduler = false;
+      // api.config.tasks.queues    = [];
 
       r.context.api = api;
 
       actionhero.start(function(){
         r.outputStream.write('\r\n\r\n');
         r.outputStream.write('*** REPL READY ***\r\n\r\n');
-        r.outputStream.write(r.prompt);
+        r.outputStream.write(prompt);
       });
 
     }, true);
